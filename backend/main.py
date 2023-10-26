a4fccd0c9fbd0eef10057cc65eadac8c82ed9cd6
Alexandre Grodent <13605399-alexandregdnt@users.noreply.gitlab.com>
2023-10-26 15:39:41 +0200

    Files structure + Docker setup

---
+++ backend/main.py
@@ -0,0 +1,55 @@
+import io
+
+import numpy as np
+from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Autorise toutes les origines
+    allow_credentials=True,
+    allow_methods=["*"],  # Autorise toutes les méthodes
+    allow_headers=["*"],  # Autorise tous les headers
+)
+
+# Charger le modèle
+model = load_model("models/mnist_model3.h5")
+
+
+@app.post("/predict/")
+async def predict_digit(file: UploadFile = UploadFile(...)):
+    try:
+        # Convertir l'image en format compatible
+        image = Image.open(io.BytesIO(await file.read())).convert("L")
+        image = image.resize((28, 28))
+
+        # Inverser les couleurs
+        image = Image.fromarray(255 - np.array(image))
+
+        image_arr = np.array(image) / 255.0
+        image_arr = image_arr.reshape(1, 28, 28, 1)
+
+        # Prédiction
+        prediction = model.predict(image_arr)
+        predicted_label = np.argmax(prediction)
+
+        return {"prediction": int(predicted_label)}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+    # return render_template("index.html")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ backend/train.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Charger les données MNIST
+(mnist_train_images, mnist_train_labels), (
+    mnist_test_images,
+    mnist_test_labels,
+) = tf.keras.datasets.mnist.load_data()
+
+# Vérifier la forme des données
+print("Forme des images d'entraînement:", mnist_train_images.shape)
+print("Forme des étiquettes d'entraînement:", mnist_train_labels.shape)
+print("Forme des images de test:", mnist_test_images.shape)
+print("Forme des étiquettes de test:", mnist_test_labels.shape)
+
+
+# Afficher les 5 premières images du jeu de données d'entraînement
+for i in range(5):
+    plt.imshow(mnist_train_images[i], cmap="gray")
+    plt.title(f"Étiquette: {mnist_train_labels[i]}")
+    plt.show()
+
+# Normalisation des images
+mnist_train_images = mnist_train_images / 255.0
+mnist_test_images = mnist_test_images / 255.0
+
+# Encodage one-hot des étiquettes
+mnist_train_labels = tf.keras.utils.to_categorical(mnist_train_labels, 10)
+mnist_test_labels = tf.keras.utils.to_categorical(mnist_test_labels, 10)
+
+# Création du modèle
+model = tf.keras.models.Sequential(
+    [
+        # Redimensionnement des images pour les adapter à un CNN
+        tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),
+        # Première couche convolutive
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Deuxième couche convolutive
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Aplatir les résultats pour les adapter à une couche DNN
+        tf.keras.layers.Flatten(),
+        # Couche dense de 128 neurones
+        tf.keras.layers.Dense(128, activation="relu"),
+        # Couche de sortie avec 10 neurones (pour 10 classes) avec activation softmax
+        tf.keras.layers.Dense(10, activation="softmax"),
+    ]
+)
+
+# Compilation du modèle
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+# Affichage du résumé du modèle
+model.summary()
+
+# Entraînement du modèle
+history = model.fit(
+    mnist_train_images,
+    mnist_train_labels,
+    epochs=10,  # Nombre de fois où le modèle verra l'ensemble des données d'entraînement
+    validation_data=(
+        mnist_test_images,
+        mnist_test_labels,
+    ),  # Pour évaluer les performances du modèle sur les données de test après chaque époque
+)
+
+# Affichage de l'historique de l'entraînement (précision et perte)
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history["accuracy"], label="Précision (entraînement)")
+plt.plot(history.history["val_accuracy"], label="Précision (validation)")
+plt.title("Précision")
+plt.xlabel("Époque")
+plt.ylabel("Précision")
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history["loss"], label="Perte (entraînement)")
+plt.plot(history.history["val_loss"], label="Perte (validation)")
+plt.title("Perte")
+plt.xlabel("Époque")
+plt.ylabel("Perte")
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+# Sauvegarder le modèle
+model.save("models/mnist_model.h5")
