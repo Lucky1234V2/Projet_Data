6adc4e9419eabdae9f0ba5bea8c26833b6a3a182
Alexandre Grodent <13605399-alexandregdnt@users.noreply.gitlab.com>
2023-10-27 15:01:17 +0200

    [FRONT] DrawingCanvas + Api route

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import DrawingCanvas from '@/components/DrawingCanvas';
 
 export default function Home() {
     return (
@@ -12,18 +12,11 @@ export default function Home() {
                 </p>
             </div>
 
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                <canvas
-                    id="canvas"
-                    width="250"
-                    height="250"
-                    className="border"
-                ></canvas>
-            </div>
+            <DrawingCanvas />
 
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
                 <a
-                    href={process.env.NEXT_PUBLIC_API_URL || '/'}
+                    href={`${process.env.NEXT_PUBLIC_API_URL}/docs`}
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     target="_blank"
                     rel="noopener noreferrer"
